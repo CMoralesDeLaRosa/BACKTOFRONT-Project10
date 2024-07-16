ca14ce9451d63411abe2dd2c0608391bfb40668e
CMoralesDeLaRosa <148538842+CMoralesDeLaRosa@users.noreply.github.com>
2024-07-16 10:09:38 +0200

    Update Login.js

--- Frontend/public/src/pages/Login/Login.js
+++ Frontend/public/src/pages/Login/Login.js
@@ -15,110 +15,6 @@ export const Login = (nodoPadre) => {
   const sectionLogin = document.createElement('section')
   sectionLogin.classList.add('flex-container', 'sectionLogin')
 
-  const handleLoginFormSubmitUser = (formData) => {
-    const existingErrors = document.querySelectorAll('.errorPLogin')
-    existingErrors.forEach((error) => error.remove())
-
-    const jsonData = {}
-    formData.forEach((value, key) => {
-      jsonData[key] = value
-    })
-
-    fetch('http://localhost:3000/api/v1/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(jsonData)
-    })
-      .then((response) => {
-        showSpinner()
-        if (response.status === 400) {
-          const errorP = document.createElement('p')
-          errorP.classList.add('errorPLogin')
-          errorP.textContent = 'El usuario o la contraseña son incorrectos.'
-          const forms = document.querySelectorAll('.form')
-          const secondForm = forms[0]
-          secondForm.appendChild(errorP)
-
-          throw new Error(
-            'Error 400: Los datos proporcionados son incorrectos.'
-          )
-        }
-        if (!response.ok) {
-          throw new Error(`Error ${response.status}: ${response.statusText}`)
-        }
-        return response.json()
-      })
-      .then((data) => {
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('user', JSON.stringify(data))
-        headerLogin()
-        const main = document.querySelector('main')
-        main.innerHTML = ''
-        Eventos(main)
-        sessionStorage.setItem('paginaActual', 'eventos')
-      })
-      .catch((error) => {
-        console.error('Error al enviar datos al servidor:', error)
-      })
-      .finally(() => {
-        hideSpinner()
-      })
-  }
-
-  const handleLoginFormSubmitAttendee = (formData) => {
-    const existingErrors = document.querySelectorAll('.errorPLogin')
-    existingErrors.forEach((error) => error.remove())
-
-    const jsonData = {}
-    formData.forEach((value, key) => {
-      jsonData[key] = value
-    })
-
-    fetch('http://localhost:3000/api/v1/attendees/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(jsonData)
-    })
-      .then((response) => {
-        showSpinner()
-        if (response.status === 400) {
-          const errorP = document.createElement('p')
-          const form = document.querySelector('.loginUsers')
-          errorP.classList.add('errorPLogin')
-          errorP.textContent = 'El usuario o la contraseña son incorrectos.'
-          const forms = document.querySelectorAll('.form')
-          const secondForm = forms[1]
-          secondForm.appendChild(errorP)
-          throw new Error(
-            'Error 400: Los datos proporcionados son incorrectos.'
-          )
-        }
-        if (!response.ok) {
-          throw new Error(`Error ${response.status}: ${response.statusText}`)
-        }
-        return response.json()
-      })
-      .then((data) => {
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('attendee', JSON.stringify(data))
-        headerLogin()
-        const main = document.querySelector('main')
-        main.innerHTML = ''
-        Eventos(main)
-        sessionStorage.setItem('paginaActual', 'eventos')
-      })
-      .catch((error) => {
-        console.error('Error al enviar datos al servidor:', error)
-      })
-      .finally(() => {
-        hideSpinner()
-      })
-  }
-
   createForm(
     sectionLogin,
     'Inicia sesión como usuario',
@@ -138,4 +34,104 @@ export const Login = (nodoPadre) => {
   )
 
   nodoPadre.appendChild(sectionLogin)
+}
+
+export const handleLoginFormSubmitAttendee = (formData) => {
+  const existingErrors = document.querySelectorAll('.errorPLogin')
+  existingErrors.forEach((error) => error.remove())
+
+  const jsonData = {}
+  formData.forEach((value, key) => {
+    jsonData[key] = value
+  })
+
+  fetch('http://localhost:3000/api/v1/attendees/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(jsonData)
+  })
+    .then((response) => {
+      showSpinner()
+      if (response.status === 400) {
+        const errorP = document.createElement('p')
+        const form = document.querySelector('.loginUsers')
+        errorP.classList.add('errorPLogin')
+        errorP.textContent = 'El usuario o la contraseña son incorrectos.'
+        const forms = document.querySelectorAll('.form')
+        const secondForm = forms[1]
+        secondForm.appendChild(errorP)
+        throw new Error('Error 400: Los datos proporcionados son incorrectos.')
+      }
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      localStorage.setItem('token', data.token)
+      localStorage.setItem('attendee', JSON.stringify(data))
+      headerLogin()
+      const main = document.querySelector('main')
+      main.innerHTML = ''
+      Eventos(main)
+      sessionStorage.setItem('paginaActual', 'eventos')
+    })
+    .catch((error) => {
+      console.error('Error al enviar datos al servidor:', error)
+    })
+    .finally(() => {
+      hideSpinner()
+    })
+}
+
+export const handleLoginFormSubmitUser = (formData) => {
+  const existingErrors = document.querySelectorAll('.errorPLogin')
+  existingErrors.forEach((error) => error.remove())
+
+  const jsonData = {}
+  formData.forEach((value, key) => {
+    jsonData[key] = value
+  })
+
+  fetch('http://localhost:3000/api/v1/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(jsonData)
+  })
+    .then((response) => {
+      showSpinner()
+      if (response.status === 400) {
+        const errorP = document.createElement('p')
+        errorP.classList.add('errorPLogin')
+        errorP.textContent = 'El usuario o la contraseña son incorrectos.'
+        const forms = document.querySelectorAll('.form')
+        const secondForm = forms[0]
+        secondForm.appendChild(errorP)
+
+        throw new Error('Error 400: Los datos proporcionados son incorrectos.')
+      }
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      localStorage.setItem('token', data.token)
+      localStorage.setItem('user', JSON.stringify(data))
+      headerLogin()
+      const main = document.querySelector('main')
+      main.innerHTML = ''
+      Eventos(main)
+      sessionStorage.setItem('paginaActual', 'eventos')
+    })
+    .catch((error) => {
+      console.error('Error al enviar datos al servidor:', error)
+    })
+    .finally(() => {
+      hideSpinner()
+    })
 }
